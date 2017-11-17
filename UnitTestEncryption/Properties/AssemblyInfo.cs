33b44aeffff5861c4bb2d3d83f98817ad3a433c6
HENRY LEONARDO LEON FAJARDO <henryleo11@hotmail.com>
2017-11-17 13:09:19 -0500

    Control de Exepciones.

---
+++ UnitTestEncryption/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestEncryption")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestEncryption")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf8e30cb-ef76-47fc-9ad9-b450f8f59160")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestEncryption/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestEncryption
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void EncryptarTest()
+        {
+            string result;
+            string resultDes;
+            Encryption.Encryption encryption = new Encryption.Encryption();
+            result = encryption.Encryptar("prueba");
+            resultDes = encryption.Decryptar(result);
+            Assert.AreEqual(resultDes, "prueba");
+        }
+
+        [TestMethod()]
+        public void DecryptarTest()
+        {
+            string result;
+            Encryption.Encryption encryption = new Encryption.Encryption();
+            result = encryption.Decryptar("Rxjw3FYWWxVXoLBfzBs5BlYzkJ3xhzfJp4xV5ZiqENcmM8wa0took0rsHT7HQ1Tt0usI5QL2QfSjPvu9Q/VqlUf1aiXt3KBpd/04eheProUE6soip0wE343qPMhD2kmX");
+            Assert.AreEqual("prueba", result);
+        }
+
+        [TestMethod()]
+        public void EncryptarTestFailure()
+        {
+            string result;
+            Encryption.Encryption encryption = new Encryption.Encryption();
+            result = encryption.Encryptar("prueba");
+            Assert.AreNotEqual("/pDUAcmDArO5INGGInOzAfG6cYBl3xnxClbIpI1SyPb1CPEoVw6", result);
+        }
+
+        [TestMethod()]
+        public void DecryptarTestFailure()
+        {
+            string result;
+            Encryption.Encryption encryption = new Encryption.Encryption();
+            result = encryption.Decryptar("Rxjw3FYWWxVXoLBfzBs5BlYzkJ3xhzfJp4xV5ZiqENcmM8wa0took0rsHT7HQ1Tt0usI59L2QfSjPvu9Q/VqlUf1aiXt3KBpd/14eheProUE6soip0wE343qPMhD2kmp");
+            Assert.AreNotEqual("prueba", result);
+        }
+    }
+}
